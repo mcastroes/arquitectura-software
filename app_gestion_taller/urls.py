328ab6037f3d9aa70fc11a203ebb19749eb32448
mcastroes <mcastroes.inf@upsa.es>
2026-03-05 13:43:42 +0100

    Práctica 4

--- app_gestion_taller/urls.py
+++ app_gestion_taller/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path
-from .views import lista_clientes, detalle_cliente
+from .views import (
+    lista_clientes, 
+    detalle_cliente, 
+    registrar_cliente, 
+    registrar_coche, 
+    registrar_servicio, 
+    buscar_coche_por_matricula, 
+    buscar_coches_de_cliente, 
+    buscar_servicios_de_coche
+)
 
 urlpatterns = [
     path('clientes/', lista_clientes, name='lista_clientes'),
     path('clientes/<int:cliente_id>/', detalle_cliente, name='detalle_cliente'),
+    path('clientes/registrar/', registrar_cliente, name='registrar_cliente'),
+    path('coches/registrar/', registrar_coche, name='registrar_coche'),
+    path('coches/matricula/<str:matricula>/', buscar_coche_por_matricula, name='buscar_coche_por_matricula'),
+    path('clientes/<int:cliente_id>/coches/', buscar_coches_de_cliente, name='buscar_coches_de_cliente'),
+    path('servicios/registrar/', registrar_servicio, name='registrar_servicio'),
+    path('coches/<int:coche_id>/servicios/', buscar_servicios_de_coche, name='buscar_servicios_de_coche'),
 ]
